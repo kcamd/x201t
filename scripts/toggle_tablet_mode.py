1426eec6b22b4b0cad445b9e3ad3883470a9fbec
Dennis Mack <kcam.de@gmail.com>
2016-02-22 19:18:32 +0100

    Adds first working version of tablet_mode function

--- scripts/toggle_tablet_mode.py
+++ scripts/toggle_tablet_mode.py
@@ -1,5 +1,40 @@
 #!/usr/bin/python3
 # this scripts rotates the screen of my x201t
 # after it is put into tablet mode.
+import subprocess
+import re
+import sys
+
+TABLET_SCREEN = "LVDS1"
+
+
+def get_wacom_ids():
+    out = subprocess.getoutput("xsetwacom list")
+    ids = re.findall("id: (\d+)", out)
+    return [int(x) for x in ids]
+
+
+def activate_tablet_mode(activate):
+    wacom_ids = get_wacom_ids()
+    if activate:
+        subprocess.run(["xrandr", "--output",
+                        TABLET_SCREEN, "--rotate", "inverted"],
+                       check=True)
+        for id in wacom_ids:
+            subprocess.run(["xsetwacom", "set", str(id), "Rotate", "half"],
+                           check=True)
+    else:
+        subprocess.run(["xrandr", "--output",
+                        TABLET_SCREEN, "--rotate", "normal"],
+                       check=True)
+        for id in wacom_ids:
+            subprocess.run(["xsetwacom", "set", str(id), "Rotate", "none"],
+                           check=True)
+
 if __name__ == "__main__":
-    pass
+    activate = False
+
+    if sys.argv[1] == "1":
+        activate = True
+
+    activate_tablet_mode(activate)
